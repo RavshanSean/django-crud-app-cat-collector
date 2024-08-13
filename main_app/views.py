15f711318659d9b11bcfd2c8fc6ca02adff1c621
RavshanSean <ravshansean1995@gmail.com>
2024-08-13 12:03:28 -0400

    new

--- main_app/views.py
+++ main_app/views.py
@@ -2,12 +2,30 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
-# Create your views here.
+class Cat:
+    def __init__(self, name, breed, description, age):
+        self.name = name
+        self.breed = breed
+        self.description = description
+        self.age = age
 
-# Define the home view function
+# Create a list of Cat instances
+cats = [
+    Cat('Lolo', 'tabby', 'Kinda rude.', 3),
+    Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
+    Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
+    Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
+]
+
+
+
 def home(request):
     return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
 
-# main_app/views.py
+
+
 def about(request):
     return render(request, 'about.html')
+
+def cat_index(request):
+    return render(request, 'cats/index.html', {'cats': cats})

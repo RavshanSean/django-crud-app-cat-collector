167ebedd7c081527cbfa1c0ae87bbd5b5093790d
RavshanSean <ravshansean1995@gmail.com>
2024-08-20 11:13:33 -0400

    done

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.urls import reverse
+from datetime import date
+#for home work new 
+from django.contrib.auth.models import User
 
 MEALS = (
     ('B', 'Breakfast'),
@@ -7,7 +10,7 @@ MEALS = (
     ('D', 'Dinner')
 )
 
-    # Add the Toy model
+
 class Toy(models.Model):
     name = models.CharField(max_length=50)
     color = models.CharField(max_length=20)
@@ -24,6 +27,8 @@ class Cat(models.Model):
     description = models.TextField(max_length=250)
     age = models.IntegerField()
     toys = models.ManyToManyField(Toy)
+    #for home work new
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
     def __str__(self):
